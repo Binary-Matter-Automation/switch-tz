b07a4b4361a47e0d7c2c0016d7ddccd326052383
Reshab Das <das.reshab48@gmail.com>
2023-03-04 17:18:19 +0530

    Add context menu option to convert datetime to local timezone (#2)

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,2 +1,18 @@
-console.log('This is the background page.');
-console.log('Put the background scripts here.');
+let convertTZContextMenuItem = {
+    "id": "convertTz",
+    "title": "Convert to local",
+    "contexts": ["all"]
+};
+
+chrome.contextMenus.create(convertTZContextMenuItem);
+
+chrome.contextMenus.onClicked.addListener((clickData, tab) => {
+    if (clickData.menuItemId === 'convertTz') {
+        // Request convert timezone of element text
+        chrome.tabs.sendMessage(tab.id, 'convert_tz_context_menu_click_element_text', { frameId: clickData.frameId }, (response) => {
+            if (!response.ok) {
+                alert('Could not convert timezone');
+            }
+        });
+    }
+});
