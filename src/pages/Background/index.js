c1fda5e3527a94c47634b1e3382af0b318ee727d
Reshab Das <das.reshab48@gmail.com>
2023-03-04 20:23:47 +0530

    Handle error in onMessage response callback (#4)

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -10,7 +10,10 @@ chrome.contextMenus.onClicked.addListener((clickData, tab) => {
     if (clickData.menuItemId === 'convertTz') {
         // Request convert timezone of element text
         chrome.tabs.sendMessage(tab.id, 'convert_tz_context_menu_click_element_text', { frameId: clickData.frameId }, (response) => {
-            if (!response.ok) {
+            if (chrome.runtime.lastError) {
+                console.log(chrome.runtime.lastError);
+                alert('An unexpected error occured');
+            } else if (!response.ok) {
                 alert('Could not convert timezone');
             }
         });
